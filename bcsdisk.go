b3981cb22fe0c034fd896a7125afa3015d21fade
eggfly <lihaohua90@gmnail.com>
2014-02-05 00:08:11 +0800

    fsnofity example
    Signed-off-by: eggfly <lihaohua90@gmnail.com>

--- bcsdisk.go
+++ bcsdisk.go
@@ -1,4 +1,37 @@
 package main
 
+import (
+	"github.com/howeyc/fsnotify"
+	"log"
+)
+
 func main() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	done := make(chan bool)
+
+	// Process events
+	go func() {
+		for {
+			select {
+			case ev := <-watcher.Event:
+				log.Println("event:", ev)
+			case err := <-watcher.Error:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Watch(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	<-done
+
+	/* ... do stuff ... */
+	watcher.Close()
 }
